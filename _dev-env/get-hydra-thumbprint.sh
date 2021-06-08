e6c8f585a304ad9585c95d6e2da6d7f7d2a98382
matthieuj <matthieuj@voodoo.io>
2021-06-08 15:25:23 +0200

    remove unused files

--- _dev-env/get-hydra-thumbprint.sh
+++
@@ -1,1 +0,0 @@
-openssl s_client -connect localhost:4444 < /dev/null 2>/dev/null | openssl x509 -fingerprint -noout -in /dev/stdin | sed 's/.*=\|://g' 
--- _dev-env/wait-for-localstack.sh
+++
@@ -1,3 +0,0 @@
-until AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_REGION=us-east-1 aws --no-cli-pager --endpoint-url=http://localhost:4566 sts get-caller-identity > /dev/null 2>&1; do
-	sleep 1
-done
